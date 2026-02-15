94a9dfb4c50591602a120c9ebec3621d970cdee4
barrosjunior26 <barrosjunior26@outlook.com.br>
2026-02-15 20:38:02 -0300

    Declaração e inicialização das variáveis, inclusão do método Clear() e todas as instruções para o aplicativo fuincionar.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace ex023
 {
@@ -6,7 +7,27 @@ namespace ex023
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Variáveis globais
+            double[] Subtracao;
+            int quantidade = 0;
+            double resultado = 0;
+
+            Console.Clear();
+
+            Console.WriteLine("Informe a quantidade de valores que deseja realizar na operação: ");
+            quantidade = Convert.ToInt16(Console.ReadLine());
+
+            Subtracao = new double[quantidade];
+
+            for (int i = 0; i < Subtracao.Length; i++)
+            {
+                Console.Write($"Informe o {i + 1}º valor: ");
+                Subtracao[i] = Convert.ToDouble(Console.ReadLine());
+            }
+
+            resultado = Subtracao.Aggregate((valor1, valor2) => valor1 - valor2);
+
+            Console.WriteLine($"O resultado é = {Math.Round(resultado, 2)}");
         }
     }
 }
--- obj/Debug/net5.0/ex023.AssemblyInfo.cs
+++ obj/Debug/net5.0/ex023.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ex023")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e95dd6a9feeef5fa6a631e6867f0f6c97d2974d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ex023")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ex023")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
